a4be1c32efb8baf10433f4b6296e4598560d70b6
aarobc <aaron.b.christiansen@gmail.com>
2013-06-16 23:26:14 +0000

    added setup script

---
+++ setup.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+ln -s ~/config/.vim/bundles.vim ~/.vim/bundles.vim
+ln -s ~/config/.bashrc ~/.bashrc
+ln -s ~/config/.vimrc ~/.vimrc
