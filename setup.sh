4b864300857092c2e2f740c9e405e418032c46be
aarobc <aaron.b.christiansen@gmail.com>
2013-06-20 15:03:32 -0600

    Made setup script more comprehensive.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
-#ln -s ~/config/.vim/bundles.vim ~/.vim/bundles.vim
+
+# back up the existing files...
+mv .vim{,_bak}
+mv .bashrc{,_bak}
+mv .vimrc{,_bak}
+
+# create the symlinks
 ln -s ~/config/.vim ~/.vim
+ln -s ~/config/.bashrc ~/.bashrc
+ln -s ~/config/.vimrc ~/.vimrc
+
+# setting the shell to use the new bashrc
+source .bashrc
+
+# using vundle for vim
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-#ln -s ~/config/.bashrc ~/.bashrc
-#ln -s ~/config/.vimrc ~/.vimrc
+
+# telling vim to install it
+vim +BundleInstall +qall
+
