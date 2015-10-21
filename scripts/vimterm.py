1c00924eb8fd3348e1412bb9a94c73283290e178
aarobc <aaron.b.christiansen@gmail.com>
2015-10-21 11:26:36 -0600

    swapped out vimterm script

--- scripts/vimterm.py
+++ scripts/vimterm.py
@@ -1,40 +1,74 @@
 #!/usr/bin/env python
-"""Edit a file in the host nvim instance."""
-import os
-import sys
 
-from neovim import attach
+import neovim, os, re, sys, time
 
-args = sys.argv[1:]
-if not args:
-    print "Usage: {} <filename> ...".format(sys.argv[0])
-    sys.exit(1)
+# Get a list of buffers that haven't been deleted. `nvim.buffers` includes
+# buffers that have had `:bdelete` called on them and aren't in the buffer
+# list, so we have to filter those out.
+def get_listed_buffers(nvim):
+    return set(buf.number for buf in nvim.buffers \
+        if nvim.eval('buflisted(%d)' % buf.number))
 
-addr = os.environ.get("NVIM_LISTEN_ADDRESS", None)
-if not addr:
-    os.execvp('nvim', args)
+# For now, treat all arguments that don't start with - or + as filenames. This
+# is good enough to recognize '-f' and `+11`, which is all this script really
+# needs right now.
+filenames = [
+    re.sub(' ', '\ ', os.path.abspath(arg))
+        for arg in sys.argv[1:] if not arg[0] in ['-', '+']
+]
 
-nvim = attach("socket", path=addr)
+try:
+    nvim_socket = os.environ["NVIM_LISTEN_ADDRESS"]
+except KeyError:
+    # If we aren't running inside a `:terminal`, just exec nvim.
+    os.execvp('nvim', sys.argv)
+    sys.exit()
 
+nvim = neovim.attach('socket', path=nvim_socket)
 
-def _setup():
-    nvim.input('<c-\\><c-n>')  # exit terminal mode
-    chid = nvim.channel_id
-    nvim.command('augroup EDIT')
-    nvim.command('au BufEnter <buffer> call rpcnotify({0}, "n")'.format(chid))
-    nvim.command('au BufEnter <buffer> startinsert'.format(chid))
-    nvim.command('augroup END')
-    nvim.vars['files_to_edit'] = args
-    for x in args:
-        nvim.command('exe "drop ".remove(g:files_to_edit, 0)')
+existing_buffers = get_listed_buffers(nvim)
 
+nvim.command('split')
+nvim.command('args %s' % ' '.join(filenames))
 
-def _exit(*args):
-    nvim.vars['files_to_edit'] = None
-    nvim.command('augroup EDIT')
-    nvim.command('au!')
-    nvim.command('augroup END')
-    nvim.session.stop()
+new_buffers = get_listed_buffers(nvim).difference(existing_buffers)
 
+for arg in sys.argv:
+    if arg[0] == '+':
+        nvim.command(arg[1:])
 
-nvim.session.run(_exit, _exit, _setup)
+# The '-f' flag is a signal that we're in a situation like a `git commit`
+# invocation where we need to block until the user is done with the file(s).
+if '-f' in sys.argv and len(new_buffers) > 0:
+    # The rule here is that the user is 'done' with the opened files when none
+    # of them are visible onscreen. This allows for use cases like hitting `:q`
+    # on a `git commit` tempfile. However, we can't just poll to see if they're
+    # visible, because using `nvim.windows`, `nvim.eval()`, or `nvim.call()`
+    # will interrupt any multi-key mappings the user may be inputting. The
+    # solution is to set a buffer-local autocmd on each opened buffer so that
+    # we only check for visibility immediately after the user either closes or
+    # hides one of the buffers.
+    channel_id = nvim.channel_id
+    for buffer in new_buffers:
+        nvim.command((
+            'autocmd BufDelete,BufHidden <buffer=%d> ' +
+            'call rpcnotify(%d, "check_buffers")'
+        ) % (buffer, channel_id))
+
+    stay_open = True
+    while stay_open:
+        nvim.session.next_message() # block until `rpcnotify` is called
+        open_buffers = [window.buffer.number for window in nvim.windows]
+        stay_open = any([buffer in open_buffers for buffer in new_buffers])
+
+    # Now that none of the opened files are visible anymore, we do a few
+    # cleanup steps before ending the script:
+    #  * Clear the arg list, since otherwise `:next` would reopen the tempfile
+    #    or whatever.
+    #  * Clear the autocmds we added, since `bdelete` just hides the buffer and
+    #    the autocmds will still be active if the user reopens the file(s).
+    #  * Delete each of the buffers we created.
+    nvim.command('argdel *')
+    for buffer in new_buffers:
+        nvim.command('autocmd! BufDelete,BufHidden <buffer=%d>' % buffer)
+        nvim.command('bdelete! %d' % buffer)
