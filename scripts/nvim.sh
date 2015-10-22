9d7cecf4dce58df4e003e4d0e99a48511b5260c4
aarobc <aaron.b.christiansen@gmail.com>
2015-10-22 10:01:01 -0600

    fixed centos install script

--- scripts/nvim.sh
+++ scripts/nvim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum -y install autoconf automake cmake gcc gcc-c++ libtool make pkgconfig unzip
-git clone https://github.com/neovim/neovim.git ~/
+git clone https://github.com/neovim/neovim.git ~/neovim
 cd ~/neovim && \
 make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX:PATH=$HOME/local" && \
 make install
