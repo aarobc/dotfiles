cd87832e9ac874bf49f01666c0ea7ac17ffa42cd
aarobc <aaron.b.christiansen@gmail.com>
2015-11-21 13:34:20 -0700

    initial work on getting set up for auto install script

---
+++ scripts/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#neovim
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim
+
+sudo apt-get install python-dev python-pip python3-dev python3-pip
