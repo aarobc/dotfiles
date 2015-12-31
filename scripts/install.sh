3c2d1a9aa4e6062f213eed89c89ae09018406ef9
aarobc <aaron.b.christiansen@gmail.com>
2015-12-30 18:27:08 -0700

    perms for compose

--- scripts/install.sh
+++ scripts/install.sh
@@ -63,3 +63,4 @@ sudo usermod -aG docker `whoami`
 #docker-compose goodness
 curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > docker-compose
 sudo mv docker-compose /usr/local/bin/
+sudo chmod +x /usr/local/bin/docker-compose
