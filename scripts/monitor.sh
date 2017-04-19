0f4ebaab9509ae2b87bd691439869a2adad8215f
aarobc <aaron.b.christiansen@gmail.com>
2017-04-19 10:19:47 -0600

    updates for mac compatability fixes and such with i3

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xe
 # Read the status of the relevant graphics adapter
 # add to /etc/udev/rules.d/80-monitor-hotplug-rules
 # KERNEL=="card0", SUBSYSTEM=="drm", ENV{DISPLAY}=":0", ENV{XAUTHORITY}="/home/.../.Xauthority", RUN+="/home/.../dotfiles/scripts/monitor.sh"
@@ -10,7 +10,7 @@ export DISPLAY=:0
 
 
 # because otherwise weird issues
-sleep 2
+sleep 3
 
 
 # make this into a function eventually so not bad
@@ -23,23 +23,22 @@ else
 fi
 
 
-# read STATUS2 < /sys/class/drm/card0-HDMI-A-2/status
-# if [ "$STATUS2" = "connected" ]; then
-#     xrandr --output HDMI2 --right-of HDMI1 --auto --screen 0
-# else
-#     xrandr --output HDMI2 --off --screen 0
-# fi
+read STATUS2 < /sys/class/drm/card0-HDMI-A-2/status
+if [ "$STATUS2" = "connected" ]; then
+    xrandr --output HDMI2 --left-of eDP1 --auto --screen 0
+else
+    xrandr --output HDMI2 --off --screen 0
+fi
 
 read STATUS3 < /sys/class/drm/card0-HDMI-A-3/status
 if [ "$STATUS3" = "connected" ]; then
     # su ac -c 'xrandr --output HDMI3 --right-of eDP1 --auto --screen 0'
-  xrandr > /home/ac/Desktop/testt
-  su ac -c 'notify-send Monitor "Connected"'
+  # su ac -c 'notify-send Monitor "Connected"'
     # xrandr --output HDMI3 --right-of eDP1 --auto --screen 0
     xrandr --output HDMI3 --right-of eDP1 --auto --screen 0
 else
     xrandr --output HDMI3 --off --screen 0
-  su ac -c 'notify-send Monitor "Disconnected"'
+  # su ac -c 'notify-send Monitor "Disconnected"'
 fi
 
 # su ac -c 'notify-send Monitor "HDMI3: $STATUS3\nHDMI1: $STATUS1"'
@@ -55,3 +54,8 @@ if [ "$DP1" = "connected" ]; then
 else
     xrandr --output DP1 --off --screen 0
 fi
+
+# after everything is reorganized, move the workspaces
+sleep 1
+# su ac -c 'notify-send python "Running"'
+su ac -c '/home/ac/dotfiles/scripts/fixLayout.py'
